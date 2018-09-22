e5cb8e412617279d3e9679610ff31d36a1c38fb5
hadentro <42660483+hadentro@users.noreply.github.com>
2018-09-22 02:44:56 -0400

    Initial designs including menu screen and first calculation
    Wanted to give us this as a base for all of us to download and work around.
    
    I built a pretty simple main menu to base myself off of, please modify to your liking and I will follow suite on my forms.
    
    I took the first calculation on the list Linear Conversions - I will also probably take another one (maybe Volume?) - please test this out and let me know if I need to fix or correct something.

---
+++ CustomCalculator/CustomCalculator/frmMenu.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CustomCalculator
+{
+    public partial class frmMenu : Form
+    {
+        public frmMenu()
+        {
+            InitializeComponent();
+        }
+
+        private void frmMenu_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnLinear_Click(object sender, EventArgs e)
+        {
+            var formLinear = new frmLinear();
+            formLinear.Show();
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+    }
+}

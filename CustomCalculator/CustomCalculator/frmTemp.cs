203538dd69261972c1d70fead9361e7cd8a04c16
Erin McMackin <mcmackinerin@gmail.com>
2018-09-24 20:28:38 -0400

    basic functionality among conversions complete

--- CustomCalculator/CustomCalculator/frmTemp.cs
+++ CustomCalculator/CustomCalculator/frmTemp.cs
@@ -36,45 +36,54 @@ namespace CustomCalculator
             {
                 if (radioCel2.Checked == true)
                 {
-
+                    calcTemp = origTemp;
+                    txtConvTemp.Text = calcTemp.ToString();
                 }
                 else if (radioFahr2.Checked == true)
                 {
-
+                    calcTemp = Math.Round((origTemp * 1.8m) + 32);
+                    txtConvTemp.Text = calcTemp.ToString();
                 }
                 else if (radioKel2.Checked == true)
                 {
-
+                    calcTemp = Math.Round(origTemp + 273m);
+                    txtConvTemp.Text = calcTemp.ToString();
                 }
             }
             else if (radioFahr1.Checked == true)
             {
                 if (radioCel2.Checked == true)
                 {
-
+                    calcTemp = Math.Round((origTemp - 32) * .5556m);
+                    txtConvTemp.Text = calcTemp.ToString();
                 }
                 else if (radioFahr2.Checked == true)
                 {
-
+                    calcTemp = origTemp;
+                    txtConvTemp.Text = calcTemp.ToString();
                 }
                 else if (radioKel2.Checked == true)
                 {
-
+                    calcTemp = Math.Round((origTemp + 459.67m) * .5556m);
+                    txtConvTemp.Text = calcTemp.ToString();
                 }
             }
             else if (radioKel1.Checked == true)
             {
                 if (radioCel2.Checked == true)
                 {
-
+                    calcTemp = Math.Round(origTemp - 273m);
+                    txtConvTemp.Text = calcTemp.ToString();
                 }
                 else if (radioFahr2.Checked == true)
                 {
-
+                    calcTemp = Math.Round((origTemp * 1.8m) - 459.67m);
+                    txtConvTemp.Text = calcTemp.ToString();
                 }
                 else if (radioKel2.Checked == true)
                 {
-
+                    calcTemp = origTemp;
+                    txtConvTemp.Text = calcTemp.ToString();
                 }
             }
 
